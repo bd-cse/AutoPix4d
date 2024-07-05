6d8a4bf38f6121500ce747fcf714675d62403825
bd-cse <brentde@cs.washington.edu>
2024-07-05 16:21:43 -0700

    Updated

--- AutoPix4d.py
+++ AutoPix4d.py
@@ -4,9 +4,25 @@ from NFLoc.folder_handling.parse_files_in_folder import _get_all_images_given_fo
 from automation_scripts.autopix import automate_pix4d, process_running_wmi
 import os
 
+# This is the script to automate stitching field images in Pix4dFields. It must
+# be supplied the directory with all of the image sets as a command line
+# argument.
+# 
+# It will name the projects in Pix4dFields after the locations found in the tif
+# EXIF data alongside a count of how many images it found in that field.
+#
+# The projects will need to be visually observed for correctness.
+#
+# If the script terminates prematurely it is most likely because Pix4d threw a
+# pop-up that prevented the auto-clicker from pressing the exit button.
+#
+# It will create a log with information about its execution and what it
+# accomplished.
+
 # Gets all the image sets
 def get_sub_directory_paths(start_directory):
-    return [os.path.join(start_directory, f) for f in os.listdir(start_directory) if os.path.isdir(os.path.join(start_directory, f))]
+    return [os.path.join(start_directory, f) for f in os.listdir(start_directory) 
+            if os.path.isdir(os.path.join(start_directory, f))]
 
 def process_all_images_in_set(folder_path : str) -> dict:
     images = _get_all_images_given_folder(folder_path)
@@ -15,7 +31,7 @@ def process_all_images_in_set(folder_path : str) -> dict:
     for image in images:
         coords.append(_get_coordinates_as_point(image))
 
-    return _make_dict_with_coordinates_list("C:\\Users\\andre\\Desktop\\Project_Test\\NFLoc\\Fields.geojson", coords)
+    return _make_dict_with_coordinates_list("AutoPix4d\\Fields.geojson", coords)
 
 def interleave_field_and_num_of_images(fields_dict : dict) -> list:
     res = []
