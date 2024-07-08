0aee6e1e80f4d43ce88bf32e951d358fbe469970
bd-cse <brentde@cs.washington.edu>
2024-07-07 21:14:07 -0700

    Updated

--- AutoPix4d.py
+++ AutoPix4d.py
@@ -2,7 +2,9 @@ from NFLoc.EXIF_handling.get_gps_data import _get_coordinates_as_point
 from NFLoc.geojson_handling.get_geojson_info import _make_dict_with_coordinates_list
 from NFLoc.folder_handling.parse_files_in_folder import _get_all_images_given_folder
 from automation_scripts.autopix import automate_pix4d, process_running_wmi
+from time import strftime
 import os
+import logging
 
 # This is the script to automate stitching field images in Pix4dFields. It must
 # be supplied the directory with all of the image sets as a command line
@@ -39,27 +41,32 @@ def interleave_field_and_num_of_images(fields_dict : dict) -> list:
         res.append(field + "-" + str(fields_dict[field]) + "tifs")
     return res
 
+if __name__ == '__main__':
+    timestamp = strftime("%Y-%m-%dT%H%M%S")
+    logging.basicConfig(filename=('AutoPix4d\\logs\\' + timestamp + '.log'), level=logging.INFO)
+    logger = logging.getLogger(__name__)
+    logger.info('Began process at ' + timestamp)
 
-start_directory = "C:\\Users\\andre\\Desktop\\Project_Test\\ideal_img_set_folder"
-tif_sets = get_sub_directory_paths(start_directory)
-res = []
+    start_directory = "C:\\Users\\andre\\Desktop\\Project_Test\\ideal_img_set_folder"
+    tif_sets = get_sub_directory_paths(start_directory)
+    res = []
 
-for set in tif_sets:
-    print("Processing images in " + set + "...")
-    img_dict = process_all_images_in_set(set)
-    field_name = str(interleave_field_and_num_of_images(img_dict))
-    print("Done for " + field_name)
+    for set in tif_sets:
+        img_dict = process_all_images_in_set(set)
+        field_name = str(interleave_field_and_num_of_images(img_dict))
+        logger.info('Retrieved EXIF data in ' + set + ' ... ' + field_name)
 
-    print("Opening Pix4d to process " + set + "...")
-    automate_pix4d(set, field_name)
+        logger.info("Opening Pix4d to create project...")
+        automate_pix4d(set, field_name)
 
-    if process_running_wmi("Pix4dfields.exe"):
-        print("Pix4d did not close properly... terminating script")
-        break
+        if process_running_wmi("Pix4dfields.exe"):
+            logger.error(set + " in Pix4d did not close as expected.")
+            break
 
-    res.append(field_name)
-    print("Completed " + set + "!")
+        res.append(set + " ... " + field_name)
 
-for field in res:
-    print("Made: " + field)
+    for field in res.reverse():
+        logger.info("Made project: " + field)
+
+    logger.info("Script terminated at " + strftime("%Y-%m-%dT%H%M%S"))
 
